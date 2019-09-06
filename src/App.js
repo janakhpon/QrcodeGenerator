c9144cac3df0e7d06976dddd704024a55aec0f7a
janakhpon <jnovaxer@gmail.com>
2019-09-06 06:36:05 +0630

    GDEV002

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import React, { Fragment } from 'react';
+import { Grid, Card } from "@material-ui/core";
+import GetInput from './Components/Input';
+import QRCode from 'qrcode.react';
 
-export default function App() {
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container fixed>
-        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
-      </Container>
-    </React.Fragment>
-  );
+
+
+export default class App extends React.Component {
+
+  state = {
+    url: '',
+    size: 256,
+    fgColor: '#000000',
+    bgColor: '#ffffff',
+    level: 'L',
+    renderAs: 'canvas',
+    includeMargin: false,
+  }
+
+
+
+
+  handleClick = (data) => {
+    this.setState({ url: data })
+    console.log('data' + this.state.url);
+  }
+
+
+  render() {
+
+    return (
+      <Fragment>
+        <Grid container justify="center" style={{ marginTop: '10rem' }}>
+          <Grid item>
+            <Card>
+              <GetInput handleGenerate={this.handleClick} />
+            </Card>
+            <Card className='qrcode'>
+              <QRCode
+                value={this.state.url}
+                size={this.state.size}
+                fgColor={this.state.fgColor}
+                bgColor={this.state.bgColor}
+                level={this.state.level}
+                renderAs={this.state.renderAs}
+                includeMargin={this.state.includeMargin} />
+            </Card>
+          </Grid>
+
+        </Grid>
+
+      </Fragment>
+    );
+  }
+
 }
